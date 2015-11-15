916974133c31ce7b13718cccc83c1e9c89eda09c
Cole R Lawrence <cole@zombiehippie.com>
2015-11-15 14:45:01 -0600

    Update for greater control over settings

--- ExtendScript.py
+++ ExtendScript.py
@@ -12,38 +12,44 @@ class ExtendScriptCommand(sublime_plugin.TextCommand):
         if s.get('save_first') and self.active_view() and self.active_view().is_dirty():
             self.active_view().run_command('save')
         msg = 'Execute '
-        base_path="c:\Program Files\Adobe"
         software_folder=""
         software_name=""
-        script_folder=base_path
+        script_folder=""
 
         if command[1] == 'ae':
-            software_folder="\Adobe After Effects "+s.get('AE_version')+"\Support Files"
+            software_folder=s.get('AE_folder')
+            software_folder += "\Support Files"
+            script_folder= software_folder+"\Scripts"
             software_name="AfterFX"
-            script_folder+=software_folder+"\Scripts"
         elif command[1] == 'ai':
-            software_folder="\Adobe Illustrator "+s.get('AI_version')+" (64 Bit)"
-            script_folder+=software_folder+"\Scripting"
-            software_folder+="\Support Files\Contents\Windows"
-            software_name="Illustrator"
+            software_folder = s.get('AI_folder')
+            script_folder = software_folder+"\Scripting"
+            software_folder += "\Support Files\Contents\Windows"
+            software_name = "Illustrator"
         elif command[1] == 'ps':
-            software_folder="\Adobe Photoshop "+s.get('PS_version')+" (64 Bit)"
+            software_folder=s.get('PS_folder')
             software_name="Photoshop"
-            script_folder+=software_folder+"\Presets\Scripts"
+            script_folder=software_folder+"\Presets\Scripts"
 
         script_folder+="\\"
 
         file_name = self.active_view().file_name()
         if s.get('coffee_mode'):
-            if(file_name.split('.')[1] == 'coffee'):
+            if(file_name.split('.')[-1] == 'coffee'):
                 file_name = file_name.split('.')[0]+'.js'
         msg+= ' Script: ' + file_name
 
         only_file_name = s.get('file_export_prefix') + file_name.split('\\')[len(file_name.split('\\'))-1]
         batch ='@echo'
-        batch+='& copy /D "' + file_name +'" "'+script_folder+only_file_name.split('.')[0]+'.jsx'+'"'
-        batch+='& cd /D "'+base_path + software_folder+'"'
-        batch+='& '+software_name+' -r "'+script_folder+only_file_name.split('.')[0]+'.jsx'+'"'
+        if s.get('copy_to_software_scripts'):
+            batch+='& copy /D "' + file_name +'" "'+script_folder+only_file_name.split('.')[0]+'.jsx'+'"'
+            batch+='& cd /D "' + software_folder+'"'
+            batch+='& '+ software_name + ' -r "' + script_folder+only_file_name.split('.')[0]+'.jsx'+'"'
+        else:
+            batch+='& cd /D "' + software_folder+'"'
+            batch+='& '+ software_name + ' -r "' + file_name +'"'
+        # For debugging batch execution
+        #batch+='& pause'
 
         os.system(batch)
 
